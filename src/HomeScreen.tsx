61145744d2d370c8b559b71bf081bc6146a7e55d
philipp <philipp.reiter90@gmail.com>
2023-03-31 10:17:07 +0200

    home screen and "lots of images screen"

---
+++ src/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { Button, StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Button title="Click me to load images" onPress={() => {navigation.navigate('LotsOfImages')}} />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
